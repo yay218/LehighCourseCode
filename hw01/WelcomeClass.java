7445963b5acef121c5061849681eed637e302528
yay218 <yiyang1996@outlook.com>
2015-01-26 00:05:16 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,19 @@
+// define a class
+public class WelcomeClass{
+    
+// add main method    
+    public static void main(String[] args) {
+
+// print the statement with box around it        
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println(" <-Y--A--Y--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("This August my wife and I took an eleven-day, 675-mile bike tour on our tandem. We circled Lake Ontario.");
+        
+    }
+}
